5bda122b2a355336d25e0a612ea89b6bc9933bef
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-03-09 15:26:54 +0100

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { TableComponent } from './table/table.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent,
+    CarouselComponent,
+    TableComponent,
+    PaginationComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule
